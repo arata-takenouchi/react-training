c2a59243fff522859268a10fbde989441a5a01e9
arata-takenouchi <arata.takenouchi.1230@gmail.com>
2025-02-14 01:14:01 +0900

    practice9

--- src/BadSampleComponent.tsx
+++ src/BadSampleComponent.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react"
+import { useEffect, useMemo, useState } from "react"
 
 // Bad
 function TodoList({todos, filter}) {
@@ -14,5 +14,18 @@ function TodoList({todos, filter}) {
 function TodoList({todos, filter}) {
   const [newTodo, setNewTodo] = useState('')
 
+  console.time('filter array')
   const visibleTodos = getFilteredTodos(todos, filter)
+  console.timeEnd('filter array')
+}
+
+// more
+function TodoList({todos, filter}) {
+  const [newTodo, setNewTodo] = useState('')
+
+  console.time('memo function')
+  const visibleTodos = useMemo(() => {
+    return getFilteredTodos(todos, filter)
+  }, [todos, filter])
+  console.timeEnd('memo function')
 }
