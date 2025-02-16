e5ef1feae8a93e3621d362374671dde25a63fea4
arata-takenouchi <arata.takenouchi.1230@gmail.com>
2025-02-16 23:46:47 +0900

    practice14

--- src/BadSampleComponent.tsx
+++ src/BadSampleComponent.tsx
@@ -1,38 +1,68 @@
 import { useEffect, useMemo, useState } from "react"
 
 // Bad
-function Form() {
-  const [firstName, setFirstName] = useState('')
-  const [lastName, setLastName] = useState('')
+function Game() {
+  const [card, setCard] = useState(null)
+  const [goldCardCount, setGoldCardCount] = useState(0)
+  const [round, setRound] = useState(1)
+  const [isGameOver, setIsGameOver] = useState(false)
 
   useEffect(() => {
-    post('/analytics/event', {  eventName: 'visit_form' })
-  }, [])
-
-  const [jsonToSubmit, setJsonToSubmit] = useState(null)
-  useEffect(() => {
-    if (jsonToSubmit !== null) {
-      post('/api/register', jsonToSubmit)
+    if (card !== null && card.gold) {
+      setGoldCardCount(c => c + 1)
     }
-  }, [jsonToSubmit])
+  }, [card])
 
-  function handleSubmit(e) {
-    e.preventDefault()
-    setJsonToSubmit({ firstName, lastName })
+  useEffect(() => {
+    if (goldCardCount > 3) {
+      setRound(r => r + 1)
+      setGoldCardCount(0)
+    }
+  }, [goldCardCount])
+
+  useEffect(() => {
+    if (round > 5) {
+      setIsGameOver(true)
+    }
+  }, [round])
+
+  useEffect(() => {
+    alert('Good Game!')
+  }, [isGameOver])
+
+  function handlePlaceCard(nextCard) {
+    if (isGameOver) {
+      throw new Error('Game already ended')
+    } else {
+      setCard(nextCard)
+    }
   }
 }
 
 // Better
-function Form() {
-  const [firstName, setFirstName] = useState('')
-  const [lastName, setLastName] = useState('')
+function Game() {
+  const [card, setCard] = useState(null)
+  const [goldCardCount, setGoldCardCount] = useState(0)
+  const [round, setRound] = useState(1)
 
-  useEffect(() => {
-    post('/analytics/event', {  eventName: 'visit_form' })
-  }, [])
+  const isGameOver = round > 5
 
-  function handleSubmit(e) {
-    e.preventDefault()
-    post('/api/register', { firstName, lastName })
+  function handlePlaceCard(nextCard) {
+    if (isGameOver) {
+      throw new Error('Game already ended')
+    }
+
+    setCard(nextCard)
+    if (nextCard.gold) {
+      if (goldCardCount <= 3) {
+        setGoldCardCount(goldCardCount + 1)
+      } else {
+        setGoldCardCount(0)
+        setRound(round + 1)
+        if (round === 5) {
+          alert('Good Game')
+        }
+      }
+    }
   }
 }
