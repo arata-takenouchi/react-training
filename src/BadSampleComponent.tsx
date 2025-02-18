173e45b1aba39acd5b498efa8006a4bb64f6f382
arata-takenouchi <arata.takenouchi.1230@gmail.com>
2025-02-18 23:44:06 +0900

    practice16

--- src/BadSampleComponent.tsx
+++ src/BadSampleComponent.tsx
@@ -1,21 +1,44 @@
 import { useEffect, useMemo, useState } from "react"
 
 // Bad
-function App() {
+function Toggle({ onChange }) {
+  const [isOn, setIsOn] = useState(false)
+
   useEffect(() => {
-    loadDataFromLocalStorage()
-    checkAuthToken()
-  }, [])
+    onChange(isOn)
+  }, [isOn, onChange])
+
+  function handleClick() {
+    setIsOn(!isOn)
+  }
+
+  function handleDragEnd(e) {
+    if (isCloserToRightEdge(e)) {
+      setIsOn(true)
+    } else {
+      setIsOn(false)
+    }
+  }
 }
 
 // Better
-let didInit = false
-function App() {
-  useEffect(() => {
-    if (!didInit) {
-      didInit = true
+function Toggle({ onChange }) {
+  const [isOn, setIsOn] = useState(false)
+
+  function updateToggle(nextIsOn) {
+    setIsOn(nextIsOn)
+    onChange(nextIsOn)
+  }
+
+  function handleClick() {
+    updateToggle(!isOn)
+  }
+
+  function handleDragEnd(e) {
+    if (isCloserToRightEdge(e)) {
+      updateToggle(true)
+    } else {
+      updateToggle(false)
     }
-    loadDataFromLocalStorage()
-    checkAuthToken()
-  }, [])
+  }
 }
