d81a1f5e557bbadd5aadcaab7d2ed4473aea33c7
arata-takenouchi <arata.takenouchi.1230@gmail.com>
2025-02-19 01:11:26 +0900

    practice18

--- src/BadSampleComponent.tsx
+++ src/BadSampleComponent.tsx
@@ -1,27 +1,48 @@
-import { useEffect, useMemo, useState } from "react"
+import { useEffect, useMemo, useState, useSyncExternalStore } from "react"
 
 // Bad
-function Parent() {
-  const [data, setData] = useState(null)
-  return <Child onFetched={setData} />
-}
-
-function Child({ onFetched }) {
-  const data = useSomeAPI()
+function useOnlineStatus() {
+  const [isOnline, setIsOnline] = useState(true)
 
   useEffect(() => {
-    if (data) {
-      onFetched(data)
+    function updateState() {
+      setIsOnline(navigator.onLine)
     }
-  }, [onFetched, data])
+
+    updateState()
+
+    window.addEventListener('online', updateState)
+    window.addEventListener('offline', updateState)
+    return () => {
+      window.removeEventListener('online', updateState)
+      window.removeEventListener('offline', updateState)
+    }
+  }, [])
+  return isOnline
+}
+
+function ChatIndecator() {
+  const isOnline = useOnlineStatus()
 }
 
 // Better
-function Parent() {
-  const data = useSomeAPI()
-  return <Child data={data} />
+function subscribe(callback) {
+  window.addEventListener('online', callback)
+  window.addEventListener('offline', callback)
+  return () => {
+    window.removeEventListener('online', callback)
+    window.removeEventListener('offline', callback)
+  }
 }
 
-function Child({ data }) {
+function useOnlineStatus() {
+  return useSyncExternalStore(
+    subscribe,
+    () => navigator.onLine,
+    () => true
+  )
+}
 
+function ChatIndicator() {
+  const isOnline = useOnlineStatus()
 }
