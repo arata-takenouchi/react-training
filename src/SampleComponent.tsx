f5fb1c0bad9403b2823c7d0ecc1f004345218d51
arata-takenouchi <arata.takenouchi.1230@gmail.com>
2025-02-20 02:01:32 +0900

    practice21

--- src/SampleComponent.tsx
+++ src/SampleComponent.tsx
@@ -3,6 +3,65 @@ import { useEffect, useState } from "react";
 function Counter() {
   const [number, setNumber] = useState(0)
 
+  const [person, setPerson] = useState({
+    name: 'name',
+    artwork: {
+      title: 'title',
+      image: 'image'
+    }
+  })
+  setPerson({
+    ...person,
+    artwork: {
+      ...person.artwork,
+      image: 'image2'
+    }
+  })
+
+  const [artists, setArtists] = useState([
+    {
+      id: 1,
+      name: 'name1'
+    },
+    {
+      id: 2,
+      name: 'name2'
+    },
+    {
+      id: 3,
+      name: 'name3'
+    },
+  ])
+
+  function handleClick() {
+    const insertAt = 1
+    const nextArtists = [
+      ...artists.slice(0, insertAt),
+      { id: artists.length + 1, name: `name${artists.length + 1}` },
+      ...artists.slice(insertAt)
+    ]
+    setArtists(nextArtists)
+  }
+
+  // array mutation
+  // Bad
+  function handleToggleList(targetId, nextValue) {
+    const nextList = [...list]
+    const artwork = nextList.find(a => a.id === targetId)
+    artwork.seen = nextValue
+    setList(nextList)
+  }
+  // Good
+  function handleToggleList(targetId, nextValue) {
+    setList(FileList.map(artwork => {
+      if (artwork.id === targetId) {
+        return { ...artwork, seen: nextValue }
+      } else {
+        return artwork
+      }
+    }))
+  }
+
   return (
     <>
       <h1>{ number }</h1>
