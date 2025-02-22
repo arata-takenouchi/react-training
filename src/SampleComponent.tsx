af30c69a50195cc3b7c431f44b045444cef99450
arata-takenouchi <arata.takenouchi.1230@gmail.com>
2025-02-22 18:20:00 +0900

    practice24

--- src/SampleComponent.tsx
+++ src/SampleComponent.tsx
@@ -1,86 +1,47 @@
 import { useEffect, useState } from "react";
 
-function Counter() {
-  const [number, setNumber] = useState(0)
+function useOnlineStatus() {
+  const [isOnline, setIsOnline] = useState(true)
 
-  const [person, setPerson] = useState({
-    name: 'name',
-    artwork: {
-      title: 'title',
-      image: 'image'
+  useEffect(() => {
+    function handleOnline() {
+      setIsOnline(true)
     }
-  })
-  setPerson({
-    ...person,
-    artwork: {
-      ...person.artwork,
-      image: 'image2'
+    function handleOffline() {
+      setIsOnline(false)
     }
-  })
+    window.addEventListener('online', handleOnline)
+    window.addEventListener('offline', handleOffline)
+    return () => {
+      window.removeEventListener('online', handleOnline)
+      window.removeEventListener('offline', handleOffline)
+    }
+  }, [])
+  return isOnline
+}
 
-  const [artists, setArtists] = useState([
-    {
-      id: 1,
-      name: 'name1'
-    },
-    {
-      id: 2,
-      name: 'name2'
-    },
-    {
-      id: 3,
-      name: 'name3'
-    },
-  ])
+function StatusBar() {
+  const isOnline = useOnlineStatus()
+  return <h1>{isOnline ? 'Online' : 'Disconnected'}</h1>
+}
 
-  function handleClick() {
-    const insertAt = 1
-    const nextArtists = [
-      ...artists.slice(0, insertAt),
-      { id: artists.length + 1, name: `name${artists.length + 1}` },
-      ...artists.slice(insertAt)
-    ]
-    setArtists(nextArtists)
-  }
+export default StatusBar
 
-  // array mutation
-  // Bad
-  function handleToggleList(targetId, nextValue) {
-    const nextList = [...list]
-    const artwork = nextList.find(a => a.id === targetId)
-    artwork.seen = nextValue
-    setList(nextList)
-  }
-  // Good
-  function handleToggleList(targetId, nextValue) {
-    setList(FileList.map(artwork => {
-      if (artwork.id === targetId) {
-        return { ...artwork, seen: nextValue }
-      } else {
-        return artwork
-      }
-    }))
+
+function SaveButton() {
+  const isOnline = useOnlineStatus()
+
+  function handleSaveClick() {
+    console.log('Progress saved')
   }
 
   return (
-    <>
-      <h1>{ number }</h1>
-      <button onClick={() => {
-        // setNumber(number + 1)
-        // setNumber(number + 1)
-        // setNumber(number + 1)
-
-        // queue
-        // setNumber(n => n + 1)
-        // setNumber(n => n + 1)
-        // setNumber(n => n + 1)
-
-        setNumber(number + 5)
-        setNumber(n => n + 1)
-        setNumber(42)
-      }}>
-        +
-      </button>
-    </>
+    <button
+      disabled={!isOnline}
+      onClick={handleSaveClick}>
+      {isOnline ? 'Save progress' : 'Reconnecting...'}
+    </button>
   )
 }
+
+export default SaveButton
