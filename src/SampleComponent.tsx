c4175f31f9b632e5624f01f1b4381af903d621d4
arata-takenouchi <arata.takenouchi.1230@gmail.com>
2025-02-24 21:39:04 +0900

    practice29

--- src/SampleComponent.tsx
+++ src/SampleComponent.tsx
@@ -1,13 +1,22 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 
+const roomId = 'general'
 const serverUrl = 'https://localhost:1234'
 
-export default function ChatRoom({ roomId }) {
+// export default function ChatRoom({ roomId }) {
+export default function ChatRoom() {
+  // const [serverUrl, setServerUrl] = useState('https://localhost:1234')
+
+  // useEffect(() => {
+  //   logVisit(roomId)
+  // }, [roomId])
+
   useEffect(() => {
     const connection = createConnection(serverUrl, roomId)
     connection.connect()
     return () => {
       connection.disconnect()
     }
-  }, [roomId])
+  }, [])
+  // }, [roomId, serverUrl])
 }
